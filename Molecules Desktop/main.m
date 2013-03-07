e59af3fd9151ed235c0114b0c61eae0a0c753b3a
Brad Larson <larson@sunsetlakesoftware.com>
2013-03-06 21:15:27 -0600

    Initial Commit

---
+++ Molecules Desktop/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Molecules Desktop
+//
+//  Created by Brad Larson on 3/6/2013.
+//  Copyright (c) 2013 Sunset Lake Software LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
