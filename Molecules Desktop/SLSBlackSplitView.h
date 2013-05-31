704606b4f7ec4702b8e9363534e699af0b485d56
Brad Larson <larson@sunsetlakesoftware.com>
2013-05-30 19:44:52 -0500

    Reworked application into a single-window architecture. Added notifications for Leap connection and disconnection.

---
+++ Molecules Desktop/SLSBlackSplitView.h
@@ -0,0 +1,13 @@
+//
+//  SLSBlackSplitView.h
+//  Molecules Desktop
+//
+//  Created by Brad Larson on 5/28/2013.
+//  Copyright (c) 2013 Sunset Lake Software LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLSBlackSplitView : NSSplitView
+
+@end
---
+++ Molecules Desktop/SLSBlackSplitView.m
@@ -0,0 +1,28 @@
+//
+//  SLSBlackSplitView.m
+//  Molecules Desktop
+//
+//  Created by Brad Larson on 5/28/2013.
+//  Copyright (c) 2013 Sunset Lake Software LLC. All rights reserved.
+//
+
+#import "SLSBlackSplitView.h"
+
+@implementation SLSBlackSplitView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
